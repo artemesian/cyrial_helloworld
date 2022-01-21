e371227812041cd4f10de69bbe0121dd6d8af551
cyrial kamda <kamdacyrial@gmail.com>
2022-01-21 13:44:02 -0500

    added random number generator for nft

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
+    clock::Clock,
     msg,
+    hash,
     borsh::{try_from_slice_unchecked},
     program::{invoke, invoke_signed}, 
     pubkey::Pubkey,
@@ -33,6 +35,54 @@ pub struct LockTime{// Find out how to use the clock on solana
     pub unlockable_date: u32
 }
 
+pub fn lock_time(counter:u32)->u32{
+    let mut e_power:f32  = (counter - 80000) as f32 / 50000.0;
+    e_power = e_power.powf(5.0);
+    e_power = 1.0 + std::f32::consts::E.powf(-e_power);
+    (365.0 * 8640.0 * e_power) as u32
+}
+
+
+
+fn select_uri<'life>(ind: u8) -> &'life str {
+
+
+
+    if ind == 0 {
+        return "https://arweave.net/OioGclZGd62Rq-9hPWgcw5yVnsiNsPMfUjOVB8cpkxI";
+    }
+    else if ind == 1 {
+        return "https://arweave.net/JBfrg0II2UnjTcqbfpBfjJkMvEiJxBL7dqy4NoJE6KY";
+    }
+    else if ind == 2 {
+       return "https://arweave.net/b2sC9VSoIsYkwWi35469R5uVR9TuqhyFaTfzA_mGK4o";
+    }
+    else if ind == 3 {
+       return "https://arweave.net/64KtiE7AxHpfSvo94WVryVtoXLkegl3UgFPNwbVvtpA";
+    }
+    else if ind == 4{
+       return "https://arweave.net/yZj1tJKobPrl3nF4lZxmb_Pv00ZKCtMDnSR88TIFYrI";
+    }
+    else if ind == 5 {
+       return "https://arweave.net/c8xNffeYluRyBq_H7REUGzzQKA6oKPPm9Utu6w4jpcI";
+    }
+    else if ind == 6{
+       return "https://arweave.net/ft3m1VjpNZ809fjeYDhwzB-Y-NJmWyNL2GjtNXYlOv4";
+    }
+    else if ind == 7{
+       return "https://arweave.net/co68y7GK5Ec2-r5ZhigioavtzFPKOQaA6HjSzRj3t2c";
+    }
+    else if ind == 8{
+       return "https://arweave.net/Tdw8rS02eK2LPqsHCH6UrtXGGDdM3DAggySKV0ONYgE";
+    }
+    else if ind == 9 {
+       return "https://arweave.net/_oowkkm0nbGiDIhLKh4niZLWLQGh_2BTZ76W0JPPXeI";
+    }
+    else if ind == 10{
+      return "https://arweave.net/s1vtegvJxHDt70oyC3Y4eK0wEzq78l6CqT8e2jdCsdA";
+    }
+       return "https://arweave.net/Y6XxIE1ft2Zxfxs3BUptdWhZqgK7VcMRjJg4APDjZ0s";
+}
 
 
 pub fn process_instructions(
@@ -40,9 +90,7 @@ pub fn process_instructions(
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    // let instruction = Instructions::unpackinst(instruction_data)?;
-    let unlockable_date: u32 = 2022;
-
+    
     let account_info_iter = &mut accounts.iter();
 
     // match instruction {
@@ -61,11 +109,19 @@ pub fn process_instructions(
     let locktime_pda_info = next_account_info(account_info_iter)?;
     let sales_pda_info = next_account_info(account_info_iter)?;
     let metadata_pda_info = next_account_info(account_info_iter)?;
+    let sysvar_clock_info = next_account_info(account_info_iter)?;
     // let associated_token_program_info = next_account_info(account_info_iter)?;
     let temp_key = Pubkey::from_str("G473EkeR5gowVn8CRwTSDop3zPwaNixwp62qi7nyVf4z").unwrap();
     if vault.key != &temp_key && program_id == program_id {
         Err(ProgramError::InvalidInstructionData)?
     }
+
+
+    let clock = Clock::from_account_info(&sysvar_clock_info)?;
+    // Getting timestamp
+    let current_timestamp = clock.unix_timestamp as u32;
+
+    // let instruction = Instructions::unpackinst(instruction_data)?;
 
     // let program_id = next_account_info(account_info_iter)?;
     let space: usize = 82;
@@ -85,6 +141,8 @@ pub fn process_instructions(
     let unitary = sales_account_data.vault_total * 1.25 / sales_account_data.counter as f32;
 
     let price = (unitary  * (i32::pow(10,9) as f32)) as u64;
+
+    let unlockable_date: u32 = current_timestamp + lock_time(sales_account_data.counter);
 
     // let rent = Rent::from_account_info(rent_account_info)?;
     msg!("Hello_0");
@@ -177,9 +235,22 @@ pub fn process_instructions(
     let mut creators = Vec::new();
     creators.push(Creator{address: *mint_authority_info.key, verified: true, share: 0});
     creators.push(Creator{address: *vault.key, verified:false, share:100});
+
+    let mut ves: [u8; 10] = [0;10];
+    let mut mult = current_timestamp as f32;
+    let mut cnt = 0;
+
+    while cnt <10 {
+        ves[cnt] = (mult as u32 % 256) as u8;
+        mult = mult / 7.0;
+        cnt += 1;
+    }
+    let new_hash = hash::hashv(&[&ves, &mint_account_info.key.to_bytes(), &program_id.to_bytes()]);
+    let index_uri: u8= new_hash.to_bytes().iter().sum();
+
     
     invoke_signed(
-        &instruction::create_metadata_accounts(id(), *metadata_pda_info.key, *mint_account_info.key, *mint_authority_info.key, *payer_account_info.key, *mint_authority_info.key, "Angelo".to_string(), "Gtree".to_string(), "https://arweave.net/PJQ_ULPglDrQQ_gyxLknq074Z8pl4w0iJCU6Atqhehk".to_string(), Some(creators), 500, true, true),
+        &instruction::create_metadata_accounts(id(), *metadata_pda_info.key, *mint_account_info.key, *mint_authority_info.key, *payer_account_info.key, *mint_authority_info.key, "Gamestree Avatar".to_string(), "Gtree".to_string(), select_uri(index_uri % 12).to_string(), Some(creators), 500, true, true),
         &[
             metadata_pda_info.clone(),
             mint_account_info.clone(),
@@ -258,7 +329,7 @@ pub fn process_instructions(
     )?;
     msg!("Hello9");
     let locktime_account_data = LockTime{
-        date_created: 2021,
+        date_created: current_timestamp,
         unlockable_date: unlockable_date,
     };
     locktime_account_data.serialize(&mut &mut locktime_pda_info.data.borrow_mut()[..])?;
@@ -292,6 +363,8 @@ pub fn process_instructions(
     // };
 
     sales_account_data.serialize(&mut &mut sales_pda_info.data.borrow_mut()[..])?;
+
+    msg!("Locked for {:?} Minutes", unlockable_date - current_timestamp);
 
     Ok(())
 }
