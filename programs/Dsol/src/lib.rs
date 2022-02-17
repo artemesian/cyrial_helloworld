913666fee689f3427ef677944d5ef86ea3482f1d
artemesian <angelodiepe10@yahoo.com>
2022-02-17 15:54:58 +0100

    added inflate_mint feature to the DSOL Program

--- programs/Dsol/src/lib.rs
+++ programs/Dsol/src/lib.rs
@@ -1,5 +1,3 @@
-use std::borrow::Borrow;
-
 use solana_program::{
     borsh::{try_from_slice_unchecked},
     account_info::{next_account_info, AccountInfo},
@@ -23,7 +21,7 @@ entrypoint!(process_instructions);
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct DsolData{
     pub date_created: u32,
-    pub next_inflation_date: u32,
+    pub last_inflation_date: u32,
 }
 
 pub enum InstructionEnum{
@@ -61,12 +59,19 @@ fn initial_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
     // Getting timestamp
     let current_timestamp = clock.unix_timestamp as u32;
     // Compute the initial next inflation timestamp after 1year
-    let next_inflation_timestamp: u32 = current_timestamp + 31536000;
+    let last_inflation_timestamp: u32 = current_timestamp;
 
-    msg!("Generating PDA");
+    msg!("Generating mint Authority PDA");
     let (mint_authority_pda, mint_authority_bump) = Pubkey::find_program_address(&[b"dsol_mint_auth_pda"], program_id);
     let mint_signers_seeds: &[&[u8]; 2] = &[b"dsol_mint_auth_pda", &[mint_authority_bump]];
     if &mint_authority_pda != mint_authority_account_info.key {
+        Err(ProgramError::InvalidAccountData)?
+    }
+
+    msg!("Generating mint PDA");
+    let (mint_pda, mint_bump) = Pubkey::find_program_address(&[b"dsol_mint_pda"], program_id);
+    let mint_pda_signer: &[&[u8]; 2] = &[b"dsol_mint_pda", &[mint_bump]];
+    if &mint_pda != mint_info.key {
         Err(ProgramError::InvalidAccountData)?
     }
 
@@ -80,7 +85,7 @@ fn initial_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
             &token_program_info.key
         ), 
         &[payer_account_info.clone(), mint_info.clone()],
-        &[]
+        &[mint_pda_signer]
     )?;
 
     msg!("Init mint account");
@@ -190,7 +195,7 @@ fn initial_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
     msg!("Save Dsol Data");
     let dsol_data_account = DsolData{
         date_created: current_timestamp,
-        next_inflation_date: next_inflation_timestamp,
+        last_inflation_date: last_inflation_timestamp,
 
     };
     dsol_data_account.serialize(&mut &mut dsol_data_pda_info.data.borrow_mut()[..])?;
@@ -198,43 +203,80 @@ fn initial_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
     Ok(())
 }
 
-fn inflate_mint(_program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
+fn inflate_mint(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult{
     let account_info_iter = &mut accounts.iter();
 
     let payer_account_info = next_account_info(account_info_iter)?;
+    let dsol_wallet_associated_token_account_info = next_account_info(account_info_iter)?;
     let mint_info = next_account_info(account_info_iter)?;
-    let dsol_data_account_info = next_account_info(account_info_iter)?;
+    let dsol_data_pda_info = next_account_info(account_info_iter)?;
+    let mint_authority_account_info = next_account_info(account_info_iter)?;
+    let token_program_info = next_account_info(account_info_iter)?;
+    let associated_account_info = next_account_info(account_info_iter)?;
+    let sysvar_clock_info = next_account_info(account_info_iter)?;
 
-    let initial_ammount = 150000000e9 as u64;
+    let dsol_data_account: DsolData = try_from_slice_unchecked(&dsol_data_pda_info.data.borrow())?;
+    let mut last_inflation_timestamp = dsol_data_account.last_inflation_date;
+    let mut amount_to_inflate: u64 = 0;
+    let clock = Clock::from_account_info(&sysvar_clock_info)?;
 
-    let dsol_data_account: DsolData = try_from_slice_unchecked(&dsol_data_account_info.data.borrow())?;
+    let current_timestamp = clock.unix_timestamp as u32;
+    let a_year_timestamp : u32 = 31536000;
 
-    // msg!("Mint to payer associated token account"); 
-    // invoke_signed(  
-    //     &spl_token::instruction::mint_to(
-    //         &token_program_info.key, 
-    //         &mint_info.key, 
-    //         &associated_account_info.key, 
-    //         &mint_authority_account_info.key, 
-    //         &[], 
-    //         150000000e9 as u64
-    //     )?, 
-    //     &[mint_info.clone(), associated_account_info.clone(), mint_authority_account_info.clone()], 
-    //     &[mint_signers_seeds]
-    // )?;
+    if &current_timestamp - &last_inflation_timestamp < a_year_timestamp  {
+        msg!("Remaining {:?} secs from now to the next inflation", &a_year_timestamp - (&current_timestamp - &last_inflation_timestamp));
+        Err(ProgramError::InvalidInstructionData)?
+    }
 
-    // msg!("Transfer tokens to Dsol wallet token account");
-    // invoke(
-    //     &spl_token::instruction::transfer(
-    //         &token_program_info.key,
-    //         &associated_account_info.key,
-    //         &dsol_wallet_associated_token_account_info.key,
-    //         &payer_account_info.key,
-    //         &[],
-    //         150000000e9 as u64
-    //     )?, 
-    //     &[associated_account_info.clone(), dsol_wallet_associated_token_account_info.clone(), payer_account_info.clone()], 
-    // )?;
+    while (&current_timestamp - &last_inflation_timestamp) >= a_year_timestamp {
+        let num_years = ((&last_inflation_timestamp + a_year_timestamp) - &dsol_data_account.date_created) / a_year_timestamp;
+        
+        let inflation_percentage = (5.0 / num_years as f32 ) / 100.0;
+        amount_to_inflate += (150000000e9 as f32 * inflation_percentage) as u64;
+
+        msg!("Year: {:?} - inflation percentage: {:?} % - amount inflation: {:?}", num_years, inflation_percentage, amount_to_inflate);
+        last_inflation_timestamp += a_year_timestamp;
+    }
+    
+
+    let (_mint_authority_pda, mint_authority_bump) = Pubkey::find_program_address(&[b"dsol_mint_auth_pda"], program_id);
+    let mint_signers_seeds: &[&[u8]; 2] = &[b"dsol_mint_auth_pda", &[mint_authority_bump]];
+
+    msg!("Mint to payer associated token account"); 
+    invoke_signed(  
+        &spl_token::instruction::mint_to(
+            &token_program_info.key, 
+            &mint_info.key, 
+            &associated_account_info.key, 
+            &mint_authority_account_info.key, 
+            &[], 
+            amount_to_inflate
+            
+        )?, 
+        &[mint_info.clone(), associated_account_info.clone(), mint_authority_account_info.clone()], 
+        &[mint_signers_seeds]
+    )?;
+
+    msg!("Transfer tokens to Dsol wallet token account");
+    invoke(
+        &spl_token::instruction::transfer(
+            &token_program_info.key,
+            &associated_account_info.key,
+            &dsol_wallet_associated_token_account_info.key,
+            &payer_account_info.key,
+            &[],
+            amount_to_inflate
+        )?, 
+        &[associated_account_info.clone(), dsol_wallet_associated_token_account_info.clone(), payer_account_info.clone()], 
+    )?;
+
+    msg!("Update Dsol Data");
+    let final_dsol_data_account = DsolData{
+        date_created: dsol_data_account.date_created,
+        last_inflation_date: last_inflation_timestamp,
+
+    };
+    final_dsol_data_account.serialize(&mut &mut dsol_data_pda_info.data.borrow_mut()[..])?;
 
     Ok(())
 
